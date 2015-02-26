5ca7771dd3ac87f265c6fb4a8ce420499eee2114
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-02-26 10:56:04 +0100

    updated script./

--- seleniumgrid/startNodes.sh
+++ seleniumgrid/startNodes.sh
@@ -4,7 +4,17 @@ docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
 docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
 docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
 docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-chrome-10:latest
 echo "Starting firefox nodes"
+docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
+docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
 docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
 docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
 docker run -d --link selenium-hub:hub hvdb/docker-node-firefox-10:latest
