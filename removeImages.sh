4c740d85f17aa60ebbcb4f37169b4a4ba3c83738
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-02-26 13:00:09 +0100

    changed name.

--- removeImages.sh
+++ removeImages.sh
@@ -1,9 +1,9 @@
 echo "Stop containers and remove them"
-docker rm -f docker-selenium-hub
-docker rm -f docker-node-chrome-10
-docker rm -f docker-node-firefox-10
+docker rm -f hvdb/docker-selenium-hub
+docker rm -f hvdb/docker-node-chrome-10
+docker rm -f hvdb/docker-node-firefox-10
 echo "Removing all images from host"
-docker rmi -f docker-selenium-hub
-docker rmi -f docker-node-chrome-10
-docker rmi -f docker-node-firefox-10
+docker rmi -f hvdb/docker-selenium-hub
+docker rmi -f hvdb/docker-node-chrome-10
+docker rmi -f hvdb/docker-node-firefox-10
 echo "Done, removing images"
