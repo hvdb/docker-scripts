02ac61976d3e485e083ba5d51f8bca9bd4b1b23b
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-02-26 13:43:04 +0100

    remove all.

--- removeImages.sh
+++ removeImages.sh
@@ -1,9 +1,5 @@
 echo "Stop containers and remove them"
-docker rm -f hvdb/docker-selenium-hub:latest
-docker rm -f hvdb/docker-node-chrome-10:latest
-docker rm -f hvdb/docker-node-firefox-10:latest
+docker rm -f $(docker ps -a -q)
 echo "Removing all images from host"
-docker rmi -f hvdb/docker-selenium-hub:latest
-docker rmi -f hvdb/docker-node-chrome-10:latest
-docker rmi -f hvdb/docker-node-firefox-10:latest
+docker rmi -f $(docker images -q)
 echo "Done, removing images"
