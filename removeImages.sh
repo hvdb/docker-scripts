36f764804173e6b242cfbbf97e7f48d3b8ddd0b7
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-02-26 12:49:24 +0100

    only remove images.

--- removeImages.sh
+++ removeImages.sh
@@ -1,5 +1,9 @@
 echo "Stop containers and remove them"
-docker rm -f $(docker ps -a -q)
+docker rm -f docker-selenium-hub
+docker rm -f docker-node-chrome-10
+docker rm -f docker-node-firefox-10
 echo "Removing all images from host"
-docker rmi -f $(docker images -q)
+docker rmi -f docker-selenium-hub
+docker rmi -f docker-node-chrome-10
+docker rmi -f docker-node-firefox-10
 echo "Done, removing images"
