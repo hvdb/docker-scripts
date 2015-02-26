313784a88548d7df9f25c986778aaf5462248c72
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-02-26 10:24:28 +0100

    fixed image for the hub

--- seleniumgrid/startHub.sh
+++ seleniumgrid/startHub.sh
@@ -1,4 +1,4 @@
 echo "starting the selenium hub"
 echo "download image and start"
-docker run -d -p 4444:4444 --name selenium-hub hvdb/hub:latest
+docker run -d -p 4444:4444 --name selenium-hub hvdb/docker-selenium-hub:latest
 echo "Done, hub is started on port 4444"
