3a0f58b141c3a7ff0d7aa3e6159f0c8c9cdbea6e
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-03-03 19:28:26 +0100

    fixed firefox

--- seleniumgrid/startNodes.sh
+++ seleniumgrid/startNodes.sh
@@ -12,16 +12,16 @@ docker run -d --link selenium-hub:hub selenium/node-chrome:2.44.0
 
 
 echo "Starting firefox nodes"
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-docker run -d --link selenium-hub:hub hvdb/node-firefox:2.44.0
-
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
+docker run -d --link selenium-hub:hub selenium/node-firefox:2.44.0
 echo "Done, nodes are started and available on the grid"
