8364fa722727e0ded6c29e0f5c96837edf9397f7
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-03-04 08:13:31 +0100

    fixed start of hub

--- seleniumgrid/startHub.sh
+++ seleniumgrid/startHub.sh
@@ -1,4 +1,4 @@
 echo "starting the selenium hub"
 echo "download image and start"
-docker run -d -p 4444:4444 --name selenium-hub -e GRID_MAX_SESSION=100 -e GRID_TIMEOUT=100000 selenium/hub:2.44.0
+docker run -d -p 4444:4444 --name selenium-hub -e GRID_MAX_SESSION=100 -e GRID_TIMEOUT=300000 selenium/hub:2.44.0
 echo "Done, hub is started on port 4444"
