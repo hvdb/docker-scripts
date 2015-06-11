8fd7c21686d4468c08767ab6434064f8fc358cf0
Henk vd Brink <henkvandenbrink@gmail.com>
2015-06-11 21:57:49 +0200

    Update removeImages.sh

--- removeImages.sh
+++ removeImages.sh
@@ -1,5 +1,5 @@
 echo "Stop containers and remove them"
-docker rm -f $(docker ps -a -q)
+docker rm -f $(docker ps -a -q | grep -e hvdb/docker-node-chrome-10 -e hvdb/docker-node-firefox-10)
 echo "Removing all images from host"
-docker rmi -f $(docker images -q)
+docker rmi -f $(docker images -q | grep -e hvdb/docker-node-chrome-10 -e hvdb/docker-node-firefox-10)
 echo "Done, removing images"
