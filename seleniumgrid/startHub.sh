f980f33acb62b88d57081e8209b0db3446609510
Henk vd Brink <henkvandenbrink@gmail.com>
2015-06-16 15:29:07 +0200

    Update startHub.sh

--- seleniumgrid/startHub.sh
+++ seleniumgrid/startHub.sh
@@ -1,4 +1,4 @@
 echo "starting the selenium hub"
 echo "download image and start"
-docker run -d -p 4444:4444 --name selenium-hub selenium/hub
+docker run -d -p 4444:4444 --name selenium-hub -e GRID_MAX_SESSION=100 selenium/hub
 echo "Done, hub is started on port 4444"
